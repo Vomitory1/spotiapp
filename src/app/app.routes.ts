0a04b95896428f2c764eb8e63bf5d6e3f520d810
jhon castro <jcastro@ias.com.co>
2018-01-19 08:04:37 -0500

    creando rutas y navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,9 @@ import { SearchComponent } from './components/search/search.component';
 
 
 const APP_ROUTES: Routes = [
-    {path: 'home', component: HomeComponent},
-    {path: 'search', component: SearchComponent},
-    {path: '', pathMatch: 'full', redirectTo: 'home'}
+    { path: 'home', component: HomeComponent },
+    { path: 'search', component: SearchComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'home' }
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
