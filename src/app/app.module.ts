b8a2af9a32db04247fbad5f9124bf86c563edfe5
jhon castro <jcastro@ias.com.co>
2018-01-29 08:40:11 -0500

    poniendo imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { APP_ROUTING } from './app.routes';
+import { SinfotoPipe } from './pipes/sinfoto.pipe';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { APP_ROUTING } from './app.routes';
     AppComponent,
     HomeComponent,
     SearchComponent,
-    NavbarComponent
+    NavbarComponent,
+    SinfotoPipe
   ],
   imports: [
     BrowserModule,
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -15,7 +15,7 @@ export class SpotifyService {
   getArtistas(termino: string) {
     const uri = `https://api.spotify.com/v1/search?query=${ termino }&type=artist&market=US&offset=0&limit=20`;
     const headers = new HttpHeaders({
-      'authorization': 'Bearer BQBaCpmY5GQGDmH-wFbJXLQeesrSARhjE6tHgBkj1S2BVRL1E1Kmj_xTUJ4XajaYmJHo6miMyEcWHqak89U'
+      'authorization': 'Bearer BQBeLCFbDJk5oR7S0GtKQ6XTs3lT1FKWX6eAohiyLjuB5oNzpk8zU3OEX5gp4m7MZpZSy1AULJ8L_YNT7_w'
     });
     return this.http.get(uri, { headers: headers })
       .map((respuesta: any) => {
