8f88a5273bfd63581736f6aafb8a36c428049066
jhon castro <jcastro@ias.com.co>
2018-02-13 08:38:14 -0500

    arreglando componente de search y artista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
-//servicios
+// servicios
 import { SpotifyService } from './services/spotify.service';
 import { HttpClientModule } from '@angular/common/http';
 
-//Component
+// Component
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { APP_ROUTING } from './app.routes';
-import { SinfotoPipe } from './pipes/sinfoto.pipe';
 import { ArtistComponent } from './components/artist/artist.component';
+
+// pipes
+import { SinfotoPipe } from './pipes/sinfoto.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+
 
 
 @NgModule({
@@ -23,6 +27,7 @@ import { ArtistComponent } from './components/artist/artist.component';
     SearchComponent,
     NavbarComponent,
     SinfotoPipe,
+    DomseguroPipe,
     ArtistComponent
   ],
   imports: [
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, OnInit, Input, Output, AfterContentInit, ContentChild,
+  AfterViewInit, ViewChild, ViewChildren, AfterViewChecked
+} from '@angular/core';
 import { SpotifyService } from '../../services/spotify.service';
+
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styles: []
 })
-export class SearchComponent {
-
+export class SearchComponent implements AfterViewInit {
+  @ViewChildren('input') vc;
   termino = '';
   access_token = '';
+
+
+  ngAfterViewInit() {
+    this.vc.first.nativeElement.focus();
+  }
 
   // Inyeccion del servicio spotify
   constructor(public _spotify: SpotifyService) {
